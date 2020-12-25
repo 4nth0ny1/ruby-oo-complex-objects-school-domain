d915b5a89bea57620659bd3414eaaac679e43052
4anth0ny1 <acatullo4@gmail.com>
2020-12-25 12:13:37 -0600

    commit tree 847723d075197a122eb3ad508f48383887fb89f1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School 
+    attr_accessor :roster  
+    
+    def initialize(name)
+      @name = name
+      @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+      if(roster[grade])
+          roster[grade] << name
+      else 
+          roster[grade] = []
+          roster[grade] << name
+      end
+      roster
+      # binding.pry
+  end
+
+  def grade(grade)
+      roster[grade]
+  end
+
+  def sort 
+      roster.each do |key, value|
+          value.sort!
+      end
+      roster.sort.to_h
+  end
+   
+  end 
